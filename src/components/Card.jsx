32aa063e8b2d12205e6c27dbae7ad974d1e53fc5
Vasanti-Vaghela <vasantivaghela2812@gmail.com>
2024-10-18 15:28:57 +0530

    add main page and few components

---
+++ src/components/Card.jsx
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types'
+const Card = ({children, bg='bg-gray-100'}) => {
+  return (
+    <div className={`${bg} p-6 rounded-lg shadow-md`}>
+      {children}
+    </div>
+  )
+}
+
+export default Card
+
+Card.propTypes = {
+    children: PropTypes.node,
+    bg:PropTypes.string
+}
